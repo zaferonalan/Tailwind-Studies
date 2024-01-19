f1d0bea804e0bff6fa9d78b5f6cb0f1801ebb253
zafergunayonalan <zafergunay7@gmail.com>
2024-01-19 20:47:01 +0300

    font ve container hazırlandı

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        gemunu: ["Gemunu Libre", "sans-serif"],
+        opne: ["Open Sans", "sans-serif"],
+      },
+    },
+
+    colors: {
+      "gega-red": "#BC1A45",
+      "gega-melon": "#FFD369",
+      "gega-gray": "#DDDDDD",
+      "gega-white": "#F7F7F7",
+    },
   },
   plugins: [],
 };
